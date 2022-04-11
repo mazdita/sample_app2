1b65c3b28a24ca74fd874ec72b882f6cef43faf6
mazdita <mazdita@hotmail.com>
2022-04-11 19:39:09 +0200

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  #get 'static_pages/../..'
+  #root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
-  root 'aplication#hello'
+  get 'static_pages/about'
+  #root 'aplication#hello'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
