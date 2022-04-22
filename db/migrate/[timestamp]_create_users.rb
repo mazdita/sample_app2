5c9818d24e8dc95099840611f2cf429c98170370
MagRegBar <magdalena.reig@jobandtalent.com>
2022-04-22 13:24:40 +0200

    make a basic User model (inlcudes password)not fixed

---
+++ db/migrate/[timestamp]_create_users.rb
@@ -0,0 +1,6 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+    def change
+        add_index :users, :email, unique: true
+        end
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_04_22_105722) do
 
 end
