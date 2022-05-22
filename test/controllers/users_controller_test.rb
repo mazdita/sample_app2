079b7a047d35e09fde3b40e2bed9254a4121e08d
mazdita <mazdita@hotmail.com>
2022-05-22 22:52:16 +0200

    add user following

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -69,5 +69,10 @@ delete user_path(@user)
 end
 assert_redirected_to root_url
 end
+test "should redirect followers when not logged in" do
+  get followers_user_path(@user)
+  assert_redirected_to login_url
+  end
+
 
 end
