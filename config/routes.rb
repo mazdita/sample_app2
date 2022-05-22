8452cd1cda8cdc00dd21be81c842b69636184786
mazdita <mazdita@hotmail.com>
2022-05-22 17:09:07 +0200

    add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
   #root 'aplication#hello'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
