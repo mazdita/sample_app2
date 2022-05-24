892a8f25fb8a500603945ac4d190e1830c30cbcf
mazdita <mazdita@hotmail.com>
2022-05-24 11:57:53 +0200

    fixing account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,8 +18,8 @@ class SessionsController < ApplicationController
         redirect_to root_url
         end
   else
-  flash.now[:danger] = 'Invalid email/password combination'
-  render 'new'
+    flash.now[:danger] = 'Invalid email/password combination'
+    render 'new'
   end
   end
 
